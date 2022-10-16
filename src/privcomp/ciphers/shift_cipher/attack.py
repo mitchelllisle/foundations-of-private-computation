2fc50064c802b4da928cc00cbf9bb92847533143
Mitchell Lisle <mitchelllisle@mailbox.org>
2022-10-16 21:35:24 +1100

    add more tests

--- src/privcomp/ciphers/shift_cipher/attack.py
+++ src/privcomp/ciphers/shift_cipher/attack.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from privcomp.ciphers.shift_cipher.cipher import ShiftCipher
 
 
@@ -6,7 +8,7 @@ class ShiftCipherAttack:
         self.ciphertext: str = ciphertext
         self.attack_space: range = range(26)
 
-    def run(self) -> list[str]:
+    def run(self) -> List[str]:
         plaintexts = []
         for shift in self.attack_space:
             shift_cipher = ShiftCipher(shift)
