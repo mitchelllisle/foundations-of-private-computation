259135d3c559319e667f5e7e2d69658fc41146b2
Mitchell Lisle <mitchelllisle@mailbox.org>
2022-10-17 19:35:50 +1100

    add more tests

--- src/privcomp/ciphers/mono_alphabetic/cipher.py
+++ src/privcomp/ciphers/mono_alphabetic/cipher.py
@@ -6,6 +6,14 @@ from privcomp.ciphers.cipher import AbstractCipher
 
 
 class MonoAlphabeticCipher(AbstractCipher):
+    """Mono Alphabetic cipher
+    We've seen that the shift cipher is easy to break as the number of possible keys was just 26. A better approach
+    would be to simply substitute each letter of the alphabet by another random letter. This way we would have 26!
+    different permutations (that is a number of the order of ). Here we cannot do exhaustive search, it would take so
+    long for our computer. A secret key could be for instance "bdeoinmkclxuqzytpwvjgafrsh", where the ciphertext for
+    "a" is "b", the ciphertext for "b" is "d" and so on.
+    """
+
     def __init__(self, secret_key: str = 'xtkcndbjipyvmfeslagourwqhz'):
         self.secret_key = secret_key
         self._secret_key_lookup = dict(zip(self.secret_key, range(len(self.secret_key))))
