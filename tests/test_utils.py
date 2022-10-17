fb6e26913957f749ce9a9177d03b0adfafbf9265
Mitchell Lisle <mitchelllisle@mailbox.org>
2022-10-17 19:25:44 +1100

    add more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,9 @@
+import string
+
+import pytest
+
 from privcomp.ciphers.cipher import AbstractCipher
+from privcomp.text_utils import letter_count
 
 
 def test_clean_text():
@@ -6,3 +11,11 @@ def test_clean_text():
     cleaned = AbstractCipher.clean_text(text)
 
     assert cleaned == 'this is a test'
+
+
+@pytest.mark.parametrize(
+    'case', [(string.ascii_lowercase, [1 for _ in string.ascii_lowercase]), ('abbcccdddd', [4, 3, 2, 1])]
+)
+def test_letter_count(case):
+    count = letter_count(case[0])
+    assert [x[1] for x in count] == case[1]
